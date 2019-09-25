34c837a22cdc4c87c58b0f629072175327a24bcf
K.-Michael Aye <kmichael.aye@gmail.com>
2019-09-25 13:21:00 -0600

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = ['pytest', ]
 
 setup(
     name='planets',
-    version='0.4.1',
+    version='0.4.2',
     description="Library to provide planetary constants",
     long_description=readme + '\n\n' + history,
     author="K.-Michael Aye",
