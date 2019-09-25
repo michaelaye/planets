6259ed8bd91ea7ef97f50ea6f04dc0f4ec315664
K.-Michael Aye <kmichael.aye@gmail.com>
2019-09-25 13:56:07 -0600

    Bump version: 0.4.3 → 0.4.4

--- planets/__init__.py
+++ planets/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """K.-Michael Aye"""
 __email__ = 'kmichael.aye@gmail.com'
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 
 from ._planets import *
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = ['pytest', ]
 
 setup(
     name='planets',
-    version='0.4.3',
+    version='0.4.4',
     description="Library to provide planetary constants",
     long_description=readme + '\n\n' + history,
     author="K.-Michael Aye",
