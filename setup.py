88220dc1b6e61e590a10d17c735a881876c704a5
K.-Michael Aye <kmichael.aye@gmail.com>
2019-09-25 13:28:44 -0600

    remove requirements.txt, add dependencies in setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.rst') as history_file:
 
 requirements = ['Click>=6.0', ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ['pytest-runner', 'spiceypy', 'spicer']
 
 test_requirements = ['pytest', ]
 
