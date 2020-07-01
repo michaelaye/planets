5e8832233a738c5804eef0ee1f39f380b08d583e
Michael Aye <kmichael.aye@gmail.com>
2020-07-01 00:25:58 -0600

    style: apply black formatting rules

--- setup.py
+++ setup.py
@@ -5,48 +5,45 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = ['Click>=6.0', 'spiceypy', 'spicer', 'astropy']
 
-setup_requirements = ['pytest-runner']
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = [
+    "pytest",
+]
 
 setup(
-    name='planets',
-    version='0.4.5',
+    name="planets",
+    version="0.4.5",
     description="Library to provide planetary constants",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     author="K.-Michael Aye",
-    author_email='kmichael.aye@gmail.com',
-    url='https://github.com/michaelaye/planets',
-    packages=find_packages(include=['planets']),
-    entry_points={
-        'console_scripts': [
-            'planets=planets.cli:main'
-        ]
-    },
-    package_dir={'planets':
-                 'planets'},
+    author_email="kmichael.aye@gmail.com",
+    url="https://github.com/michaelaye/planets",
+    packages=find_packages(include=["planets"]),
+    entry_points={"console_scripts": ["planets=planets.cli:main"]},
+    package_dir={"planets": "planets"},
     include_package_data=True,
     install_requires=requirements,
     license="MIT license",
     zip_safe=False,
-    keywords='planets',
+    keywords="planets",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
     setup_requires=setup_requirements,
 )
